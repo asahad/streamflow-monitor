eebbc86f8097acf2df52508d90b37508136d8cdb
Alhassan Sahad <98456443+asahad@users.noreply.github.com>
2024-01-27 23:09:48 -0600

    Add leaflet CSS dynamically

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,15 +1,32 @@
-import { useState } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"; // Ensure Marker is included here
+import  { useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
 import { Container, Row, Col } from "react-bootstrap";
 import streamGauge from "./streamGauge";
 import "leaflet/dist/leaflet.css";
 import "../style/custom.css";
+import markerIconPng from "leaflet/dist/images/marker-icon.png";
+import markerShadowPng from "leaflet/dist/images/marker-shadow.png";
+
+// Define the custom marker icon
+const customMarkerIcon = new L.Icon({
+  iconUrl: markerIconPng,
+  iconRetinaUrl: markerIconPng,
+  shadowUrl: markerShadowPng,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+
 const HomePage = () => {
   const [selectedSiteName, setSelectedSiteName] = useState("");
+
   const GaugeMarker = ({ gage }) => {
     return (
       <Marker
         position={[gage.SiteLatitude, gage.SiteLongitude]}
+        icon={customMarkerIcon} // Use the custom marker icon
         eventHandlers={{
           click: () => {
             setSelectedSiteName(gage.SiteName);
